5ae4dfa640e2964272efb4b31bc94176bc9c2d56
Wei <wzzang@users.noreply.github.com>
2020-08-03 11:42:17 -0500

    Add files via upload

---
+++ demo_chunk.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+filename='~/Downloads/BJ_PM25.csv'
+FileReader = pd.read_csv(filename, chunksize=100, sep = ',')
+dfList=[]
+
+for df in FileReader:
+    dfList.append(df)
+
+#concatenate all chunks
+chunkdf = pd.concat(dfList,sort=False)
+
+chunkdf.head()
+chunkdf.shape 
